cce712063c0d389b57c2431f2fe04bf7cf7d8695
saulhidalgoaular <saulhidalgoaular@gmail.com>
2011-07-08 18:41:49 +0000

    Agregada ventana de login. Agregado menu principal de Total Pos Administrativo

--- Total POS/src/totalpos/Main.java
+++ Total POS/src/totalpos/Main.java
@@ -8,9 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
         StartSplash splash = new StartSplash();
-        splash.setVisible(true);
         Shared.centerFrame(splash);
+        splash.setVisible(true);
         splash.changeStatus(Constants.appName, 10);
 
+        Login login = new Login();
+        Shared.centerFrame(login);
+        login.setVisible(true);
+
+        splash.setVisible(false);
+        
     }
 }

e17351d4b8303c0d10c19aaffc3f732091c2a378
saulhidalgoaular@gmail.com <saulhidalgoaular@gmail.com@f0d06cc6-4e9e-b11a-d304-2fce348b89af>
2011-07-13 21:39:51 +0000

    Avances en la conexión con la base de datos para usuarios.

--- trunk/Total POS/src/totalpos/Constants.java
+++ trunk/Total POS/src/totalpos/Constants.java
@@ -7,4 +7,11 @@ package totalpos;
 public class Constants {
     public static final String companyName = "Grupo Total 99";
     public static final String appName = "Total POS";
+
+    public static final int numberConnection = 5;
+    public static final String dbHost = "127.0.0.1";
+    public static final String dbName = "gt99";
+    public static final String dbUser = "root";
+    public static final String dbPassword = "123456789";
+
 }
--- trunk/Total POS/src/totalpos/Shared.java
+++ trunk/Total POS/src/totalpos/Shared.java
@@ -2,6 +2,10 @@ package totalpos;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,6 +23,19 @@ public class Shared {
         int x = (dim.width-w)/2;
         int y = (dim.height-h)/2;
         frame.setLocation(x, y);
+    }
+
+    protected static String hashPassword(String x){
+        try {
+            MessageDigest d = MessageDigest.getInstance("SHA-1");
+            d.reset();
+            d.update(x.getBytes());
+            return d.digest().toString();
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Shared.class.getName()).log(Level.SEVERE, null, ex);
+            return x;
+        }
     }
 
     protected static boolean login(String user, String password){

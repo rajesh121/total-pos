224569cee7356d60866b2b69937186794cf8aef0
saulhidalgoaular <saulhidalgoaular@gmail.com>
2011-07-20 13:45:26 +0000

    Usuario se bloquea luego de 3 intentos. Muestra el mensaje correspondiente si el usuario no existe

--- Total POS/src/totalpos/Constants.java
+++ Total POS/src/totalpos/Constants.java
@@ -9,10 +9,12 @@ public class Constants {
     public static final String appName = "Total POS";
 
     public static final int numberConnection = 5;
+
     public static final String dbHost = "127.0.0.1";
     public static final String dbName = "gt99";
     public static final String dbUser = "root";
     public static final String dbPassword = "123456789";
     public static final String defaultUser = "Invitado";
 
+    public static final String wrongPasswordMsg = "Contraseña errónea.";
 }

1679870feee4e183b6452b72a833f660813f9f1c
saulhidalgoaular@gmail.com <saulhidalgoaular@gmail.com@f0d06cc6-4e9e-b11a-d304-2fce348b89af>
2011-08-25 21:30:48 +0000

    Working in time to finish...

--- trunk/Total POS/src/totalpos/Constants.java
+++ trunk/Total POS/src/totalpos/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
     public static final boolean isPos = true;
 
     public static int secondToCheckTurn = 30;
+    public static int secondToUpdateCountdown = 5;
 
     /**
      * TODO: This must be in the local database!!!
